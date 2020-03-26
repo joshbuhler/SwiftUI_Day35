e3dc1928b46c3fa1ae6fe5e5392975f0c1398740
Joshua Buhler <josh@buhler.me>
2020-03-26 16:38:38 -0600

    Initial Commit

---
+++ Day_35_challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day_35_challenge
+//
+//  Created by Joshua Buhler on 3/26/20.
+//  Copyright © 2020 jb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Working files for iOS Team SwiftUI Lurnins
+
+https://www.hackingwithswift.com/guide/ios-swiftui/3/3/challenge
