3c1d200d269cb1df90e45642b1dbc07132eea49f
Joshua Buhler <josh@buhler.me>
2020-03-26 21:45:44 -0600

    Moving views into separate files

--- Day_35_challenge/ContentView.swift
+++ Day_35_challenge/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
         case .setup:
             return AnyView(SetupView(presenter: self.presenter))
         case .playing:
-            return AnyView(PlayingView())
+            return AnyView(GameView(presenter: self.presenter))
         case .gameOver:
             return AnyView(GameOverView())
         }
@@ -27,85 +27,6 @@ struct ContentView: View {
     }
 }
 
-struct SetupView: View {
-    
-    var presenter:MathGamePresenter
-    
-    @State private var lowerBound:Int = 0
-    @State private var upperBound:Int = 5
-    @State private var totalRounds:Int = 0
-    
-    var roundOptions:[String] = ["5", "10", "20", "All"]
-    
-    func startGame () {
-        let rounds:Int = Int(roundOptions[totalRounds]) ?? 0
-        
-        presenter.startGame(totalQuestions: rounds,
-                            upperRange: upperBound,
-                            lowerRange: lowerBound)
-    }
-    
-    var body: some View {
-        
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.orange, Color.black]),
-                           startPoint: .top,
-                           endPoint: .bottom).edgesIgnoringSafeArea(.all)
-            
-            VStack (spacing: 30) {
-                Text("MATH FACTS")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                
-                Text("Set the game range:")
-                
-                HStack {
-                    VStack {
-                        Text("Lowest Number: \(self.lowerBound)")
-                        Stepper("", value: $lowerBound, in: 0...12)
-                        .labelsHidden()
-                    }
-                    
-                    Spacer()
-                    
-                    VStack (alignment: .center) {
-                        Text("Highest Number: \(self.upperBound)")
-                        Stepper("", value: $upperBound, in: 0...12)
-                            .labelsHidden()
-                    }
-                }
-                
-                Picker("Total Rounds", selection: $totalRounds) {
-                    ForEach (0 ..< roundOptions.count) {
-                        Text("\(self.roundOptions[$0])")
-                    }
-                }.pickerStyle(SegmentedPickerStyle())
-                
-                Button(action: {
-                    self.startGame()
-                }) {
-                    Image(systemName: "airplane")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .padding()
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .foregroundColor(Color.white)
-                }
-                
-                Spacer ()
-            }.padding()
-            
-            
-        }.foregroundColor(Color.white)
-    }
-}
-
-struct PlayingView: View {
-    var body: some View {
-        Text("Playing Game")
-    }
-}
 
 struct GameOverView: View {
     var body: some View {
---
+++ Day_35_challenge/SetupView.swift
@@ -0,0 +1,83 @@
+//
+//  SetupView.swift
+//  Day_35_challenge
+//
+//  Created by Joshua Buhler on 3/26/20.
+//  Copyright © 2020 jb. All rights reserved.
+//
+
+import SwiftUI
+
+struct SetupView: View {
+    
+    var presenter:MathGamePresenter
+    
+    @State private var lowerBound:Int = 0
+    @State private var upperBound:Int = 5
+    @State private var totalRounds:Int = 0
+    
+    var roundOptions:[String] = ["5", "10", "20", "All"]
+    
+    func startGame () {
+        let rounds:Int = Int(roundOptions[totalRounds]) ?? 0
+        
+        presenter.startGame(totalQuestions: rounds,
+                            upperRange: upperBound,
+                            lowerRange: lowerBound)
+    }
+    
+    var body: some View {
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.orange, Color.black]),
+                           startPoint: .top,
+                           endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack (spacing: 30) {
+                Text("MATH FACTS")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                Text("Set the game range:")
+                
+                HStack {
+                    VStack {
+                        Text("Lowest Number: \(self.lowerBound)")
+                        Stepper("", value: $lowerBound, in: 0...12)
+                        .labelsHidden()
+                    }
+                    
+                    Spacer()
+                    
+                    VStack (alignment: .center) {
+                        Text("Highest Number: \(self.upperBound)")
+                        Stepper("", value: $upperBound, in: 0...12)
+                            .labelsHidden()
+                    }
+                }
+                
+                Picker("Total Rounds", selection: $totalRounds) {
+                    ForEach (0 ..< roundOptions.count) {
+                        Text("\(self.roundOptions[$0])")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+                
+                Button(action: {
+                    self.startGame()
+                }) {
+                    Image(systemName: "airplane")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .padding()
+                    .background(Color.green)
+                    .clipShape(Circle())
+                    .foregroundColor(Color.white)
+                }
+                
+                Spacer ()
+            }.padding()
+            
+            
+        }.foregroundColor(Color.white)
+    }
+}
