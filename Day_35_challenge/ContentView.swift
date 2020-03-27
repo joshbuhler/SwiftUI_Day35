daf9b313b52bffeab9e7b75d25274e64db2ec9dd
Joshua Buhler <josh@buhler.me>
2020-03-26 20:32:49 -0600

    Stubbed views

--- Day_35_challenge/ContentView.swift
+++ Day_35_challenge/ContentView.swift
@@ -9,8 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var presenter:MathGamePresenter = MathGamePresenter()
+    @State var gameState:GameState = .setup
+    @State var option:Int = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        switch (self.gameState) {
+        case .setup:
+            return AnyView(SetupView())
+        case .playing:
+            return AnyView(PlayingView())
+        case .gameOver:
+            return AnyView(GameOverView())
+        }
+        
+    }
+}
+
+struct SetupView: View {
+    var body: some View {
+        Text("Setup Game").onTapGesture {
+            print ("hi")
+        }
+    }
+}
+
+struct PlayingView: View {
+    var body: some View {
+        Text("Playing Game")
+    }
+}
+
+struct GameOverView: View {
+    var body: some View {
+        Text("Game Over")
     }
 }
 
